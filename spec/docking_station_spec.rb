b9a50a32e5cb6b03f10f28d3246816bb9aadd2e5
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-10-19 17:23:26 +0200

    Challenge 8 - back to the unit (add release_bike method)

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,10 @@
 require "docking_station"
 
 describe DockingStation do
+  it 'release bike' do
+    docking_station = DockingStation.new
+    expect(docking_station).to respond_to(:release_bike).with(0).argument
+  end
+  
 
 end
