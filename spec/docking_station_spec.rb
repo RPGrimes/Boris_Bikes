0cb4b60e0749483d7864a910fbd096acaff9db17
Courtney Stow <courtney-stow@hotmail.co.uk>
2021-10-19 17:02:44 +0100

    challenge 9 - building a bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,7 @@ require "docking_station"
 describe DockingStation do
   it 'release bike' do
     docking_station = DockingStation.new
-    expect(docking_station).to respond_to(:release_bike).with(0).argument
+    expect(docking_station).to respond_to(:release_bike)
   end
   
 
